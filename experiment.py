f42a264389254d684807b77f7f3c40290ddd9362
nick-casa <nc16@princeton.edu>
2023-11-01 11:20:24 -0400

    fix

--- experiment.py
+++ experiment.py
@@ -1,14 +1,5 @@
 from voyager import Voyager
 
-# You can also use mc_port instead of azure_login, but azure_login is highly recommended
-azure_login = {
-    "client_id": "102fbeb2-2ed7-4a82-8392-d2da26802371",
-    "redirect_url": "https://127.0.0.1/auth-response",
-    "secret_value": "FUV8Q~Fdc.D-Mv0AysXv.A6LwAp-Qb37YNOn1cIL",
-    "version": "fabric-loader-0.14.18-1.19", # the version Voyager is tested on
-}
-openai_api_key = "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"
-
 # First instantiate Voyager with skill_library_dir.
 voyager = Voyager(
     #azure_login=azure_login,
